ac7667222c75caef3f09d963e894db85c4c5ef05
Snyco <hello@snyco.uk>
2024-02-18 23:52:25 +0800

    Updated indentation to tabs.

--- functions/connection.py
+++ functions/connection.py
@@ -8,41 +8,41 @@ from typing import Dict, Optional, Union
 from aiohttp import ClientSession
 
 class Session:
-    def __init__(self, token: Optional[str], server_conf: Optional[Dict[Union[int, str], str]]):
-        self.token = token
-        self.server_conf = server_conf or json.load(open("./resources/server_conf.json", "r")) or {
-            "auth": "auth.snys.snyco.uk",
-            1: "1.snys.snyco.uk",
-            2: "2.snys.snyco.uk",
-            3: "3.snys.snyco.uk",
-            4: "4.snys.snyco.uk"
-        }
-        json.dump(self.server_conf, open("./resources/server_conf.json", "w")) if self.server_conf != json.load(open("./resources/server_conf.json", "r")) else None
+	def __init__(self, token: Optional[str], server_conf: Optional[Dict[Union[int, str], str]]):
+		self.token = token
+		self.server_conf = server_conf or json.load(open("./resources/server_conf.json", "r")) or {
+			"auth": "auth.snys.snyco.uk",
+			1: "1.snys.snyco.uk",
+			2: "2.snys.snyco.uk",
+			3: "3.snys.snyco.uk",
+			4: "4.snys.snyco.uk"
+		}
+		json.dump(self.server_conf, open("./resources/server_conf.json", "w")) if self.server_conf != json.load(open("./resources/server_conf.json", "r")) else None
 
-    async def authenticate(self, auth_server: Optional[str]) -> str:
-        ...
+	async def authenticate(self, auth_server: Optional[str]) -> str:
+		...
 
-    async def create_user(self, asterisk: str, username: str, auth_server: Optional[str], direct_connection: Optional[bool] = False) -> str:
-        public_key = ECC().generate_keypair()
+	async def create_user(self, asterisk: str, username: str, auth_server: Optional[str], direct_connection: Optional[bool] = False) -> str:
+		public_key = ECC().generate_keypair()
 
-        json.dump(self.server_conf, "./resources/server_conf.json") if Path("./resources/server_conf.json") else None
+		json.dump(self.server_conf, "./resources/server_conf.json") if Path("./resources/server_conf.json") else None
 
-        async with ClientSession() as session:
-            async with session.request(
-                method = "POST",
-                url = auth_server,
-                json = {
-                    "username": username,
-                    "public_key": public_key,
-                    "server_conf": self.server_conf
-                }
-            ) as response:
-                if response.status in (200, 201):
-                    response_json = response.json()
-                    self.user = User(
-                        username = username,
-                        asterisk = asterisk,
-                        session_token = response_json.get("session_token"),
-                        direct_connection = direct_connection
-                    )
-                    return response_json.get("session_token")
+		async with ClientSession() as session:
+			async with session.request(
+				method = "POST",
+				url = auth_server,
+				json = {
+					"username": username,
+					"public_key": public_key,
+					"server_conf": self.server_conf
+				}
+			) as response:
+				if response.status in (200, 201):
+					response_json = response.json()
+					self.user = User(
+						username = username,
+						asterisk = asterisk,
+						session_token = response_json.get("session_token"),
+						direct_connection = direct_connection
+					)
+					return response_json.get("session_token")
--- functions/user.py
+++ functions/user.py
@@ -8,20 +8,20 @@ from pathlib import Path
 from functions.keys import ECC
 
 class User:
-    def __init__(
-            self,
-            asterisk: str,
-            username: str,
-            session_token: Optional[str],
-            direct_connection: Optional[bool] = False
-        ):
-        self.asterisk = asterisk
-        self.username = username
-        self.session_token = session_token
-        self.direct_connection = direct_connection
+	def __init__(
+			self,
+			asterisk: str,
+			username: str,
+			session_token: Optional[str],
+			direct_connection: Optional[bool] = False
+		):
+		self.asterisk = asterisk
+		self.username = username
+		self.session_token = session_token
+		self.direct_connection = direct_connection
 
-    def fetch_local(self):
-        user_data: dict | None = json.load(open("./resources/user_data.json")) if Path("./resources/user_data.json").is_file() else None
-        self.asterisk: str | None = user_data.get("asterisk")
-        self.username: str | None = user_data.get("username")
-        self.direct_connection: str | None = user_data.get("direct_connection")
+	def fetch_local(self):
+		user_data: dict | None = json.load(open("./resources/user_data.json")) if Path("./resources/user_data.json").is_file() else None
+		self.asterisk: str | None = user_data.get("asterisk")
+		self.username: str | None = user_data.get("username")
+		self.direct_connection: str | None = user_data.get("direct_connection")
